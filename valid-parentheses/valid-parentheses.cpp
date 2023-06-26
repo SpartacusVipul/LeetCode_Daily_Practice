b7a2f24c86f1c8bcb920c905679a03f724aee4c6
spartacus <spartacus.vipul@gmail.com>
2023-06-26 16:59:27 -0400

    Time: 4 ms (40.35%) | Memory: 6.3 MB (39.16%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // can use vector as a stack, as it provides push back and pop_back methods
+    // basic idea is every opening bracket must be closed by a corresponding closing bracket
+    // so if it is a opening bracket then push_back
+    // else check if the last pushed bracket matches the new bracket
+    // if not return false
+    bool isValid(string s) {
+        vector<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push_back(s[i]);
+            } else {
+                if (st.size() == 0) {
+                    return false;
+                }
+                char topElement = st[st.size() - 1];
+                st.pop_back();
+                if ((topElement == '(' && s[i] != ')') || (topElement == '[' && s[i] != ']') || (topElement == '{' && s[i] != '}')) {
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+};

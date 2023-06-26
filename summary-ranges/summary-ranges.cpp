69a1a40ab44bc67290b3046435694919ceb1b9c1
spartacus <spartacus.vipul@gmail.com>
2023-06-26 16:38:00 -0400

    Time: 0 ms (100.00%) | Memory: 6.8 MB (55.00%) - LeetSync

--- summary-ranges/summary-ranges.cpp
+++ summary-ranges/summary-ranges.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
+    // basic idea is we merge the consecutive numbers into windows
+    // if the window size is 1 then just the element
+    // if greater than 1, then in left->right format
+    // left is start index, right is end index
+    // if left and right are same then 1 len window
+    // if not, then other case
+    // update left to next of right
     vector<string> summaryRanges(vector<int>& nums) {
-        
         int left = 0, right = 0;
         vector<string> res;
-        
         while (left < nums.size()) {
             int right = left;
             while (right < nums.size() - 1 && nums[right] + 1 == nums[right + 1]) {

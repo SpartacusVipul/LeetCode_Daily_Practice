2ce3900687979e84f9fbe839eacc70ddcab5c314
spartacus <spartacus.vipul@gmail.com>
2023-06-29 16:51:24 -0400

    Time: 62 ms (42.98%) | Memory: 15.3 MB (44.64%) - LeetSync

--- minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    // opt(i, j) represents min ascii sum till index i in s1 and j in s2
+    // base cases are 
+    // if we reach end of any string, it impliues, we need to delete all other characts of other string
+    // so we loop till the end of the other string and add the sum and return the sum
+    // other wise, if at any point characters are same, then just increase index1m, index2.
+    // otherwise check all options, increasing index1 or index2 and taking min
+    // therefore opt(i,j) = opt(i + 1, j + 1) if s1[i] == s2[j]
+    // else opt(i,j) = min(s1[i] + opt(i + 1, j), s2[j] + opt(i, j+1))
+    // base case is opt(i,j) = sum(j over till s2.size()) if i >= s1.size()
+    // opt(i,j) = sum(i over till s1.size()) if j >= s2.size()
     int mem(int index1, int index2, string s1, string s2, vector<vector<int>> &dp) {
         if (index1 >= s1.size()) {
             int sum = 0;
@@ -34,6 +44,7 @@ public:
     }
 
     int minimumDeleteSum(string s1, string s2) {
+        //initial value of dp is very important here, for tabulation case
         vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
         // return mem(0, 0, s1, s2, dp);
         return tab(s1, s2, dp);
@@ -42,16 +53,19 @@ public:
     int tab(string s1, string s2, vector<vector<int>> &dp) {
         // base cases 
         int sum = 0;
+        // here we are filling dp[s1.size()][s2.size() - 1]
         for (int i = s2.size() - 1; i >= 0; i--) {
             dp[s1.size()][i] = sum + s2[i];
             sum += s2[i];
         }
         sum = 0;
+        // here we are filling dp[s1.size() - 1][s2.size()]
         for (int i = s1.size() - 1; i >= 0; i--) {
             dp[i][s2.size()] = sum + s1[i];
             sum += s1[i];
         }
-
+        // there might be a case we need dp[s1.size()][s2.size()] 
+        // which must be 0, logically, so either set is to zero or initialize the array to 0
         for (int index1 = s1.size() - 1; index1 >= 0; index1--) {
             for (int index2 = s2.size() - 1; index2 >= 0; index2--) {
                 int res = INT_MAX;

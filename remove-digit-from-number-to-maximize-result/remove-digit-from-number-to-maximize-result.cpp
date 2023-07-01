56f749ed2569368754c2b8610130de3736ce885b
spartacus <spartacus.vipul@gmail.com>
2023-07-01 02:29:29 -0400

    Time: 0 ms (100.00%) | Memory: 6.9 MB (54.77%) - LeetSync

--- remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // trick is to use the max function as it is used here on strings
+    // it works here because of order 
+    // it checks char by character 
     string removeDigit(string number, char digit) {
         string res = "";
         for (int i = 0; i < number.size(); i++) {

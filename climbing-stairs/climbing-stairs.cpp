c4a366f82acd3020fa2806054721280accb08623
spartacus <spartacus.vipul@gmail.com>
2023-07-02 17:16:54 -0400

    Time: 0 ms (100.00%) | Memory: 6 MB (56.25%) - LeetSync

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     // given n >= 1, also, we are currently at step 0, we 
     // want to go to step n
+    // requirement is to count number of distinct ways to climb to the top
+    // opt(i) represnts the number of distinct ways to climbt to step i 
+    // now i can be reached by i - 1 step or i - 2 step
+    // so we add up number of ways to reach i - 1 and i - 2 
+    // so opt(i) = opt(i - 1) + opt(i - 2)
     vector<int> dp = vector<int>(46, -1);
     int climbStairsMem(int n) {
         if (n <= 1) {

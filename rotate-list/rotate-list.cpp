aae9f0895fe9e88ce191997f5ddcc23f96d7cebe
spartacus <spartacus.vipul@gmail.com>
2023-07-03 19:03:24 -0400

    Time: 7 ms (75.37%) | Memory: 11.6 MB (98.99%) - LeetSync

--- rotate-list/rotate-list.cpp
+++ rotate-list/rotate-list.cpp
@@ -21,39 +21,32 @@ public:
         }
         return size;
     }
+    // maybe triicky
+    // first calculate the length of the list
+    // then check if k >= len, if yes, take mod len to know how many rotations to perform
+    // once that is done, iterate oveer the list listize - k - 1 times to go the the node,
+    // the next node of this node is the new head
+    // save the new head, make the next of the current node to NULL(closing the list here)
+    // update dummy to newHead and iterate till next null is found, once there, make next point to head,
     ListNode* rotateRight(ListNode* head, int k) {
-        if (k == 0) {
-            return head;
-        }
-        if (head == NULL) {
-            return head;
-        }
-
+        if (head == NULL) return head;
         int listSize = getListSize(head);
-        if (listSize == 1) {
-            return head;
-        }
-        cout << listSize << endl;
-        if (k >= listSize) {
-            k = k % listSize;
-        }
-        if (k == 0) {
-            return head;
-        }
+        if (listSize == 1) return head;
+
+        if (k >= listSize) k = k % listSize;
+        if (k == 0) return head;
+
         ListNode *dummy = head;
         for (int i = 0; i < listSize - k - 1; i++) {
             dummy = dummy->next;
         }
-        cout << dummy->val << endl;
-
-        ListNode *tempDummy = new ListNode(0);
-        tempDummy->next = dummy->next;
-        ListNode *newHead = dummy->next;
-        dummy->next = NULL;
-        while (tempDummy->next != NULL) {
-            tempDummy = tempDummy->next;
+        ListNode *newHead = dummy->next; // saving new head
+        dummy->next = NULL; // closing list jusst before new head
+        dummy = newHead; // updating dummy to new head, and iterating till next null is encountered
+        while (dummy->next != NULL) {
+            dummy = dummy->next;
         }
-        tempDummy->next = head;
+        dummy->next = head; // making last node point to head, instead of NULL
         return newHead;
     }
 };

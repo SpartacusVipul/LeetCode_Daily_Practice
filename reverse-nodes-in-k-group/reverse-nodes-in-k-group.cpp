69826dda85a61354630954c3a29f12f54c3269aa
spartacus <spartacus.vipul@gmail.com>
2023-07-03 18:20:27 -0400

    Time: 12 ms (94.55%) | Memory: 11.4 MB (89.94%) - LeetSync

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -26,7 +26,11 @@ public:
         }
         cout << endl;
     }
-
+    // tricky problem,
+    // reverse in groups, if nodes of group size is not present stop
+    // using heloper function getKthNode to check if the k nodes exist
+    // if exist, then we get kthNode
+    // we have to reverse from dummy->next to kth node (including)
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode *dummy = new ListNode(0);
         dummy->next = head;
@@ -34,10 +38,17 @@ public:
         
         while (true) {
             ListNode *kthNode = getKthNode(dummyRef, k);
+            // if kth node is not present, then break
             if (kthNode == NULL) {
                 break;
             }
+            // savinf next node of kth node, 
+            // because, when we are reversing from dummy->next to kth node included, 
+            // the loop should stop at next node of kth node so that kth node is also reversed
             ListNode *groupNext = kthNode->next;
+
+            // prev is made ot point to kthNode->next, cur is dummyRef->next
+            // prev is expected, cur should point to next of kth node at first everytime in the loop
             ListNode *prev = kthNode->next, *cur = dummyRef->next;
             while (cur != groupNext) {
                 ListNode *nextNode = cur->next;
@@ -45,7 +56,12 @@ public:
                 prev = cur;  
                 cur = nextNode; 
             }
-            
+            // at the end of above while loop, the kth node will be the first starting point, 
+            // looping till next of kth node, such that all from dummy->next to kthnode are reveresed
+            // now, dummyRef was initiallly pointing to start of the node which in the reversed part moved inside 
+            // save the dummyRef->next which will be the starting point for the next iteration
+            // now dummyRef->next is set to kthNode which became the new starting point
+            // after that, dummyRef is made equal to temp from initial, which is the starting point 
             ListNode *temp = dummyRef->next;
             dummyRef->next = kthNode;
             dummyRef = temp;

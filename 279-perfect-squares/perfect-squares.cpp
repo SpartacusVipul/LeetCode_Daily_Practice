1f75168859075fe5e622e7952dd78672b06181fc
spartacus <spartacus.vipul@gmail.com>
2023-09-15 20:27:22 -0400

    Time: 888 ms (5.00%) | Memory: 9.9 MB (24.97%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    // similar to other take not take problems 
+    // similar to coin change
     int mem(int n, vector<int> &dp) {
         if (n == 0) return 0;
         if (n < 0) return 1e8;
         if (dp[n] != -1) return dp[n];
-        
+
         int minCount = 1e8;
         for (int i = 1; i <= n; i++) {
             int temp = 1e8;

1249a391575cab4ffb7315e274fadfca3c49a74c
spartacus <spartacus.vipul@gmail.com>
2023-09-15 20:26:57 -0400

    Time: 894 ms (5.00%) | Memory: 9.9 MB (27.22%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mem(int n, vector<int> &dp) {
+        if (n == 0) return 0;
+        if (n < 0) return 1e8;
+        if (dp[n] != -1) return dp[n];
+        
+        int minCount = 1e8;
+        for (int i = 1; i <= n; i++) {
+            int temp = 1e8;
+            if (i * i <= n) {
+                temp = 1 + mem(n - (i * i), dp);
+            }
+            if (temp < minCount) minCount = temp;
+        }
+        return dp[n] = minCount;
+
+    }
+
+    int numSquares(int n) {
+        if (n == 1) return 1;
+        vector<int> dp(n + 1, -1);        
+        return mem(n, dp);
+    }
+};

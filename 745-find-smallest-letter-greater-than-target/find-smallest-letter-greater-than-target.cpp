797e2c8da9452fe523115b67dc36622a03266630
spartacus <spartacus.vipul@gmail.com>
2023-09-21 20:21:29 -0400

    Time: 14 ms (37.58%) | Memory: 16.2 MB (9.16%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
+    // if even in the end nothing is found that is higher than target
+    // just return the first character 
     char nextGreatestLetter(vector<char>& letters, char target) {
         int left = 0, right = letters.size() - 1;
-        char res = letters[0];
-        while (left <= right) {
+        while (left < right) {
+            cout << left << " " << right << endl;
             int mid = left + (right - left) / 2;
             if (letters[mid] > target) {
-                res = letters[mid];
-                right = mid - 1;
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return res;
+        return letters[left] > target ? letters[left] : letters[0];
     }
 };

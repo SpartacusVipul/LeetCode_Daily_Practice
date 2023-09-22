0bf172d2f34f4136da0d27fa375d0d71936d189d
spartacus <spartacus.vipul@gmail.com>
2023-09-22 01:18:08 -0400

    Time: 7 ms (53.35%) | Memory: 14.1 MB (7.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    // really need to check when to use <= and < conditions in the while loop 
+    // this issue with < or <= is solved in opt function below
+    // really need to check when to use <= and < conditions in the while loop  
     // basic idea is calling the binary search function twice
     // first time to find the starting position, another time to find the ending position
     // so when target is equal to mid, if we want to find the starting position, 
@@ -29,9 +30,36 @@ public:
         return index;
     }
 
-    vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> searchRangeOld(vector<int>& nums, int target) {
         int start = binarySearchBiased(nums, target, 1);
         int end =  binarySearchBiased(nums, target, 0);
+        return {start, end};
+    }
+
+    int binarySearchBiasedOpt(vector<int> &nums, int target, int bias) {
+        int left = 0, right = nums.size();
+        int index = -1;
+        while (left < right) {
+            int mid = left +  (right - left) / 2;
+            // cout << left << " " << right << " " << mid << endl;
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (bias == 1) {
+                left = mid + 1;
+                index = mid;
+            } else {
+                right = mid;
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = binarySearchBiasedOpt(nums, target, 0);
+        int end =  binarySearchBiasedOpt(nums, target, 1);
         return {start, end};
     }
 };
